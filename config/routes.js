00b0a03b10184cdcdc728350dddc4f361289b08b
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-14 08:50:18 -0700

    login and register working

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const { authenticate } = require('../auth/authenticate');
+const Users = require('../users/users-model.js')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,43 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+         res.status(201).json(saved)
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(500).json(error)
+    })
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+  
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        res.status(500).json(error);
+      });
+
 }
 
 function getJokes(req, res) {
@@ -30,3 +66,21 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // standard claim = sub => subject
+    username: user.username,
+    roles: ['student'],
+    
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
